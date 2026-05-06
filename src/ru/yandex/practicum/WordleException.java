fe41f2a29f10c5256a83d0b593da6c02a71680ff
AntonKellerman <kellermankupriyanov@yandex.ru>
2026-05-06 12:33:47 +0300

    Игра Wordle v1.3

--- src/ru/yandex/practicum/WordleException.java
+++ src/ru/yandex/practicum/WordleException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum;
 
-public class WordleException extends Exception{
+public class WordleException extends Exception {
 
     public WordleException(String message) {
         super(message);
